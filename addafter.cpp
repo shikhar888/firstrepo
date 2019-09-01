f8a5b046f509f55615488bfc35ecce70e1007f81
shikhar888 <48325022+shikhar888@users.noreply.github.com>
2019-09-01 16:18:55 +0530

    Create addafter.cpp

---
+++ addafter.cpp
@@ -0,0 +1,94 @@
+#include<conio.h>
+#include<iostream>
+using namespace std;
+struct node
+{
+	int info;
+	node *link;
+};
+node *start=NULL;
+void create()
+{
+	node *temp,*p;
+	int n,i=1;
+	cout<<"enter no. of nodes: ";
+	cin>>n;
+	temp=new node();
+	cout<<"enter node data ";
+	cin>>temp->info;
+	temp->link=NULL;
+	start=temp;
+	p=temp;
+	for(i=2;i<=n;i++)
+	{
+		temp=new node();
+		cout<<"enter next node data ";
+		cin>>temp->info;
+		p->link=temp;
+		p=p->link;
+	}
+	p->link=NULL;
+}
+void insert_beg()
+{
+	node *temp = new node();
+	if(start==NULL)
+	{
+		cout<<"list is empty\n";
+	}
+		cout<<"\nenter new data for beginning ";
+		cin>>temp->info;
+	    temp->link=start;
+    	start=temp;	
+}
+void insert_after()
+{
+	int n;
+	node *ptr,*temp;
+	cout<<"\nenter data after which the next new node is to be inserted  ";
+	cin>>n;
+	ptr=start;
+	while(ptr!=NULL)
+	{
+		if(ptr->info==n)
+		{
+			temp=new node();
+			cout<<"enter data ";
+			cin>>temp->info;
+			temp->link=ptr->link;
+			ptr->link=temp;
+			break;
+		}
+		else
+		{
+			ptr=ptr->link;
+		}
+	}
+	if(ptr==NULL)
+	{
+		cout<<"\nNo such element found\n";
+	}
+}
+void display()
+{
+	node *temp,*p;
+	p=start;
+	cout<<"elements are\n";
+	while(p!=NULL)
+	{
+		cout<<p->info<<"  ";
+		p=p->link;
+	}
+}
+int main()
+{
+	create();
+	insert_beg();
+	display();
+	insert_beg();
+	display();
+	insert_after();
+	display();
+	getch();
+    return 0;
+}
